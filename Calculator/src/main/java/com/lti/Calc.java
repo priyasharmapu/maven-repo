9e18b7d897ebdec2e8f7235305f50d8d47205e99
Priya Sharma <priyaa.ps.31@gmail.com>
2020-12-21 15:02:38 +0530

    added subs method

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -5,9 +5,17 @@ public class Calc {
 	public static int add(int num1, int num2){
 		return num1+num2;
 	}
+	
+	public static int subs(int num1, int num2){
+		return num1-num2;
+	}
+	
+	
+	
+	
 	public static void main(String[] args) {
-		System.out.println(add(10,20));
-
+		System.out.println("Sum ="+add(10,20));
+		System.out.println("Difference ="+subs(90,20));
 	}
 
 }
