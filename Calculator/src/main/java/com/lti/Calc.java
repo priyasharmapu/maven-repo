8f6ab7a6ee6ed514d0202cb706c2bbe720c452a8
Priya Sharma <priyaa.ps.31@gmail.com>
2020-12-21 15:19:51 +0530

    div mehtod added

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -14,11 +14,16 @@ public class Calc {
 		return num1*num2;
 	}
 	
+	public static int div(int num1, int num2){
+		return num1/num2;
+	}
+	
 	
 	public static void main(String[] args) {
 		System.out.println("Sum ="+add(10,20));
 		System.out.println("Difference ="+subs(50,20));
 		System.out.println("Product ="+prod(10,20));
+		System.out.println("Quotient ="+div(10,20));
 	}
 
 }
